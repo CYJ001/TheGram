e51e91304e31e25a07208e45e6131ac98c0d4338
Chanel Johnson <cyj001@fb.com>
2017-06-26 16:11:19 -0700

    Initial Commit

---
+++ TheGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheGram
+//
+//  Created by Chanel Johnson on 6/26/17.
+//  Copyright © 2017 Chanel Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
